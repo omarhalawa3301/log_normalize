2c82db8dd2ff38017eec16e51bcf2164e5e1f663
Omar Halawa <113780419+omarhalawa3301@users.noreply.github.com>
2022-10-09 01:39:02 -0700

    Update log_normalize.py

--- src/log_normalize.py
+++ src/log_normalize.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from log_normalize_functions import *
-from log_normalize_params import *
 
 """
     Name:          Omar Halawa
@@ -9,15 +8,16 @@ from log_normalize_params import *
     File name:     log_normalize.py
     Project:       Module Development Primer, Part 2
     Description:   log_normalize is a script takes in an input GCT file, checks
-                   for its validity in both existence and file type using 
-                   log_normalize_command_line, & log normalizes all its values
-                   by utilizing logic and methods in log_normalize_functions. 
+                   for its validity in both existence and file type & log 
+                   normalizes all its values by utilizing logic and methods in
+                   log_normalize_functions. 
     References:    https://github.com/genepattern/genepattern-python  
 """
 
 
 # Checking for valid file name and extension
-if (valid):
+# Uses getter method to obtain boolean on file validity
+if (get_valid()):
     # If so, reading file into DataFrame in_df
     in_df = read(args.filename)
 
@@ -38,12 +38,14 @@ if (valid):
         out_df.to_csv(file, sep='\t', mode='w+')
 
 # Else, if only the file type, or extension, is incorrect
-elif (file_exist):
+# Uses getter method to obtain boolean on filename validity
+elif (get_filename_exist()):
     print("Error!")
     print("File type (extension) incorrect, please use a .gct file")
 
 # Else, if only the file name is incorrect
-elif (valid_ext):
+# Uses getter method to obtain boolean on file extension validity
+elif (get_valid_ext()):
     print("Error!")
     print("File name incorrect, please use a valid file in the directory")
     
